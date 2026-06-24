47f71268a85ebac14738052db068303dfc67ca39
unije <unije@DESKTOP-VCJUD63>
2026-06-24 22:19:22 +0500

    code

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -1,2 +1,181 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static int ReadInt(string message, bool allowZero = false)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                if (int.TryParse(Console.ReadLine(), out int value) && (allowZero ? value >= 0 : value > 0))
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Ошибка ввода. Введите положительное целое число.");
+            }
+        }
+        static void PrintFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+
+            Console.WriteLine($"\nСодержимое файла {fileName}:");
+
+            Console.WriteLine(File.ReadAllText(fileName));
+        }
+
+
+        public static void PrintFileBinary(string fileName)
+        {
+            using (FileStream fileStream = new FileStream(
+                fileName,
+                FileMode.Open,
+                FileAccess.Read))
+            {
+                using (BinaryReader reader = new BinaryReader(fileStream))
+                {
+                    while (fileStream.Position < fileStream.Length)
+                    {
+                        Console.WriteLine(reader.ReadInt32());
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] _)
+        {
+            const string NumbersPath = "numbers.txt";
+            const string TextPath = "text.txt";
+            const string BinaryPath = "binary.dat";
+            const string ResultPath = "result.txt";
+            const string ToysPath = "toys.xml";
+
+            while (true)
+            {
+                Console.WriteLine("\n\n--- Задание 1");
+                Console.WriteLine("1. Создать файлы");
+                Console.WriteLine("2. Получить новый файл, уменьшив каждый элемент исходного на 1.");
+                Console.WriteLine("3. Просмотр содержимого файлов");
+
+                Console.WriteLine("--- Задание 2");
+                Console.WriteLine("4. Найти разность первого и максимального элементов в файле.");
+                Console.WriteLine("--- Задание 3");
+                Console.WriteLine("5. Переписать в другой файл строки, начинающиеся с буквы б");
+                Console.WriteLine("--- Задание 4");
+                Console.WriteLine("6. Найти разность первого и максимального элементов файла");
+                Console.WriteLine("--- Задание 5");
+                Console.WriteLine("7. Определить стоимость кукол для детей шести лет");
+                Console.WriteLine("--- Задание 6");
+                Console.WriteLine("8. Оставить в списке только первые вхождения одинаковых элементов");
+                Console.WriteLine("--- Задание 7");
+                Console.WriteLine("9. Если у элемента со значением E \"соседи\" не равны, поменять их местами");
+                Console.WriteLine("--- Задание 8");
+                Console.WriteLine("10. Задание с работниками фирмы");
+                Console.WriteLine("--- Задание 9");
+                Console.WriteLine("11. Определить с каких букв начинаются слова");
+                Console.WriteLine("0. Выход");
+
+                var choice = ReadInt("Выбор: ", true);
+
+                switch (choice)
+                {
+                    case 1:
+                        Worker.FillFileNumbers(NumbersPath, 20);
+                        Worker.FillFileText(TextPath, 20);
+                        Worker.FillFileBinary(BinaryPath, 20);
+                        Worker.FillFileXML(ToysPath);
+
+                        break;
+                    case 2:
+                        Worker.DecreaseByOne(NumbersPath, ResultPath);
+
+                        break;
+                    case 3:
+                        try
+                        {
+                            Console.WriteLine("Просмотр файлов numbers.txt, result.txt: ");
+
+                            PrintFile(NumbersPath);
+                            PrintFile(TextPath);
+                            PrintFileBinary(BinaryPath);
+                            PrintFile(ResultPath);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Ошибка: {e.Message}");
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine(Worker.DifferenceFirstAndMax(NumbersPath));
+
+                        break;
+                    case 5:
+                        Worker.CopyLinesStartingWithB(TextPath, ResultPath);
+
+                        break;
+                    case 6:
+                        Worker.BinaryDifferenceFirstAndMax(BinaryPath);
+
+                        break;
+                    case 7:
+                        decimal cost =
+                            Worker.GetDollsCostForSixYears(ToysPath);
+
+                        Console.WriteLine(
+                            $"Стоимость кукол для детей 6 лет: {cost} руб.");
+
+                        break;
+                    case 8:
+                        List<int> list1 = Worker.CreateList(10);
+
+                        Console.WriteLine("Исходный список:");
+                        Worker.PrintList(list1);
+
+                        Worker.LeaveFirstOccurrences(list1);
+
+                        Console.WriteLine("После удаления повторов:");
+                        Worker.PrintList(list1);
+
+                        break;
+                    case 9:
+                        LinkedList<int> list2 = Worker.CreateLinkedList(10);
+
+                        Console.WriteLine("Исходный список:");
+                        Worker.PrintList(list2);
+
+                        var E = ReadInt("Введите E", true);
+                        Worker.SwapNeighborsOfE(list2, E);
+
+                        Console.WriteLine("После обработки:");
+                        Worker.PrintList(list2);
+
+                        break;
+                    case 10:
+                        Worker.PrintHashSetAnswers();
+
+                        break;
+                    case 11:
+                        string fileName = "students.txt";
+
+                        Worker.GenerateStudentsFile(fileName);
+
+                        List<Student> students =
+                            Worker.ReadStudentsFile(fileName);
+
+                        Worker.ProcessSchool50(students);
+
+                        break;
+                    case 0:
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ Lab7/Worker.cs
@@ -0,0 +1,568 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lab7
+{
+    public struct Toy
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+    }
+
+    public record class Student(string LastName, string FirstName, int School, int Score);
+
+    internal class Worker
+    {
+        public static void FillFileNumbers(string fileName, int count)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    writer.WriteLine(Random.Shared.Next(-100, 101)); // числа от -100 до 100
+                }
+            }
+            Console.WriteLine("Файл создан.");
+        }
+
+        public static void FillFileText(string fileName, int count)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    int length = Random.Shared.Next(5, 11);
+                    char[] chars = new char[length];
+
+                    for (int j = 0; j < length; j++)
+                    {
+                        chars[j] = (char)Random.Shared.Next('А', 'Я' + 1);
+                    }
+
+                    writer.WriteLine(new string(chars));
+                }
+            }
+        }
+
+        public static void FillFileBinary(string fileName, int count)
+        {
+            using (FileStream fileStream = new FileStream(
+                fileName,
+                FileMode.Create,
+                FileAccess.Write))
+            {
+                using (BinaryWriter writer = new BinaryWriter(fileStream))
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        writer.Write(Random.Shared.Next(-100, 101));
+                    }
+                }
+            }
+        }
+
+        public static void DecreaseByOne(string sourceFile, string resultFile)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(resultFile))
+                {
+                    foreach (string line in File.ReadLines(sourceFile))
+                    {
+                        if (int.TryParse(line, out int number))
+                        {
+                            writer.WriteLine(number - 1);
+                        }
+                    }
+                }
+                Console.WriteLine("Новый файл создан.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+        }
+
+        public static int DifferenceFirstAndMax(string fileName)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    var line = reader.ReadLine()!;
+
+                    int first = int.Parse(line);
+                    int max = first;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        int number = int.Parse(line);
+
+                        if (number > max)
+                        {
+                            max = number;
+                        }
+                    }
+
+                    return first - max;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+                return 0;
+            }
+        }
+
+        public static void CopyLinesStartingWithB(string sourceFileName, string resultFileName)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(resultFileName))
+                {
+                    foreach (string line in File.ReadLines(sourceFileName))
+                    {
+                        if (!string.IsNullOrWhiteSpace(line) &&
+                            char.ToLower(line[0]) == 'б')
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+                Console.WriteLine("Новый файл создан.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+        }
+
+        public static int BinaryDifferenceFirstAndMax(string fileName)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(
+                fileName,
+                FileMode.Open,
+                FileAccess.Read))
+                {
+                    using (BinaryReader reader = new BinaryReader(fileStream))
+                    {
+                        int firstNumber = reader.ReadInt32();
+                        int maxNumber = firstNumber;
+
+                        while (fileStream.Position < fileStream.Length)
+                        {
+                            int currentNumber = reader.ReadInt32();
+
+                            if (currentNumber > maxNumber)
+                            {
+                                maxNumber = currentNumber;
+                            }
+                        }
+
+                        return firstNumber - maxNumber;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+                return 0;
+            }
+        }
+
+        #region XML
+        public static void FillFileXML(string fileName)
+        {
+            List<Toy> toys =
+            [
+                new Toy
+                {
+                    Name = "Кукла",
+                    Price = 1500,
+                    MinAge = 3,
+                    MaxAge = 7
+                },
+                new Toy
+                {
+                    Name = "Машинка",
+                    Price = 900,
+                    MinAge = 5,
+                    MaxAge = 10
+                },
+                new Toy
+                {
+                    Name = "Кукла",
+                    Price = 2000,
+                    MinAge = 6,
+                    MaxAge = 8
+                },
+                new Toy
+                {
+                    Name = "Конструктор",
+                    Price = 2500,
+                    MinAge = 6,
+                    MaxAge = 12
+                }
+            ];
+
+            XmlSerializer serializer =
+                new XmlSerializer(typeof(List<Toy>));
+
+            using (FileStream stream = new FileStream(
+                fileName,
+                FileMode.Create))
+            {
+                serializer.Serialize(stream, toys);
+            }
+        }
+
+        public static decimal GetDollsCostForSixYears(string fileName)
+        {
+            try
+            {
+                XmlSerializer serializer =
+                new XmlSerializer(typeof(List<Toy>));
+
+                using (FileStream stream = new FileStream(
+                    fileName,
+                    FileMode.Open))
+                {
+                    List<Toy> toys =
+                        (List<Toy>)serializer.Deserialize(stream)!;
+
+                    decimal totalCost = 0;
+
+                    foreach (Toy toy in toys)
+                    {
+                        if (toy.Name == "Кукла" &&
+                            toy.MinAge <= 6 &&
+                            toy.MaxAge >= 6)
+                        {
+                            totalCost += toy.Price;
+                        }
+                    }
+
+                    return totalCost;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+                return 0;
+            }
+            
+        }
+        #endregion
+
+        #region List
+        public static List<int> CreateList(int count)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Add(Random.Shared.Next(1, 11));
+            }
+
+            return numbers;
+        }
+
+        public static void PrintList(ICollection<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void LeaveFirstOccurrences(List<int> numbers)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!uniqueNumbers.Add(numbers[i]))
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+        #endregion
+
+        #region LinkedList
+        public static LinkedList<int> CreateLinkedList(int count)
+        {
+            Random random = new Random();
+
+            LinkedList<int> numbers = new LinkedList<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers.AddLast(random.Next(1, 11));
+            }
+
+            return numbers;
+        }
+
+        public static void SwapNeighborsOfE(LinkedList<int> numbers, int e)
+        {
+            LinkedListNode<int>? current = numbers.Find(e);
+
+            if (current is null ||
+                current.Previous is null ||
+                current.Next is null)
+            {
+                return;
+            }
+
+            if (current.Previous.Value != current.Next.Value)
+            {
+                int temp = current.Previous.Value;
+                current.Previous.Value = current.Next.Value;
+                current.Next.Value = temp;
+            }
+        }
+        #endregion
+
+        #region HashSet
+        static HashSet<string> _allLanguages =
+        [
+            "Английский",
+            "Немецкий",
+            "Французский",
+            "Испанский",
+            "Китайский"
+        ];
+
+        static HashSet<string> _employee1 =
+        [
+            "Английский",
+            "Немецкий",
+            "Французский"
+        ];
+
+        static HashSet<string> _employee2 =
+        [
+            "Английский",
+            "Испанский"
+        ];
+
+        static HashSet<string> _employee3 =
+        [
+            "Английский",
+            "Немецкий"
+        ];
+
+        public static HashSet<string> GetKnownByEveryone(
+            HashSet<string> employee1,
+            HashSet<string> employee2,
+            HashSet<string> employee3)
+        {
+            HashSet<string> result = new HashSet<string>(employee1);
+
+            result.IntersectWith(employee2);
+            result.IntersectWith(employee3);
+
+            return result;
+        }
+
+        public static HashSet<string> GetKnownByAtLeastOne(
+            HashSet<string> employee1,
+            HashSet<string> employee2,
+            HashSet<string> employee3)
+        {
+            HashSet<string> result = new HashSet<string>(employee1);
+
+            result.UnionWith(employee2);
+            result.UnionWith(employee3);
+
+            return result;
+        }
+        public static HashSet<string> GetKnownByNobody(
+            HashSet<string> allLanguages,
+            HashSet<string> knownByAtLeastOne)
+        {
+            HashSet<string> result =
+                new HashSet<string>(allLanguages);
+
+            result.ExceptWith(knownByAtLeastOne);
+
+            return result;
+        }
+
+        public static void PrintLanguages(
+            string title,
+            HashSet<string> languages)
+        {
+            Console.WriteLine(title);
+
+            foreach (string language in languages)
+            {
+                Console.WriteLine(language);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void PrintHashSetAnswers()
+        {
+            HashSet<string> knownByEveryone =
+                GetKnownByEveryone(
+                    _employee1,
+                    _employee2,
+                    _employee3);
+
+            HashSet<string> knownByAtLeastOne =
+                GetKnownByAtLeastOne(
+                    _employee1,
+                    _employee2,
+                    _employee3);
+
+            HashSet<string> knownByNobody =
+                GetKnownByNobody(
+                    _allLanguages,
+                    knownByAtLeastOne);
+
+            PrintLanguages(
+                "Знают все работники:",
+                knownByEveryone);
+
+            PrintLanguages(
+                "Знает хотя бы один работник:",
+                knownByAtLeastOne);
+
+            PrintLanguages(
+                "Не знает никто:",
+                knownByNobody);
+        }
+        #endregion
+
+        #region Student
+        public static void ProcessSchool50(List<Student> students)
+        {
+            var school50 = new List<Student>();
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].School == 50)
+                {
+                    school50.Add(students[i]);
+                }
+            }
+
+            var maxScore = -1;
+            for (int i = 0; i < school50.Count; i++)
+            {
+                if (school50[i].Score > maxScore)
+                {
+                    maxScore = school50[i].Score;
+                }
+            }
+
+            var best = new List<Student>();
+            for (int i = 0; i < school50.Count; i++)
+            {
+                if (school50[i].Score == maxScore)
+                {
+                    best.Add(school50[i]);
+                }
+            }
+
+            if (best.Count > 2)
+            {
+                Console.WriteLine(best.Count);
+                return;
+            }
+
+            if (best.Count == 2)
+            {
+                Console.WriteLine($"{best[0].LastName} {best[0].FirstName}");
+                Console.WriteLine($"{best[1].LastName} {best[1].FirstName}");
+                return;
+            }
+
+            var secondScore = -1;
+            for (int i = 0; i < school50.Count; i++)
+            {
+                if (school50[i].Score < maxScore && school50[i].Score > secondScore)
+                {
+                    secondScore = school50[i].Score;
+                }
+            }
+
+            var secondCount = 0;
+            for (int i = 0; i < school50.Count; i++)
+            {
+                if (school50[i].Score == secondScore)
+                {
+                    secondCount++;
+                }
+            }
+
+            if (best.Count > 0)
+            {
+                Console.WriteLine($"{best[0].LastName} {best[0].FirstName}");
+            }
+        }
+
+        public static void GenerateStudentsFile(string fileName)
+        {
+            List<Student> students =
+            [
+                new("Иванов", "Иван", 15, 82),
+                new("Петров", "Петр", 12, 91),
+                new("Сидоров", "Алексей", 15, 77),
+                new("Кузнецов", "Максим", 8, 95),
+                new("Смирнова", "Анна", 12, 88),
+                new("Смирнова", "Анна", 50, 88),
+                new("Иванов", "Сергей", 50, 100),
+                new("Петров", "Иван", 50, 95),
+                new("Сидоров", "Алексей", 50, 90),
+                new("Кузнецов", "Павел", 50, 80),
+                new("Смирнов", "Дмитрий", 50, 70),
+                new("Орлов", "Илья", 12, 100)
+            ];
+
+            using StreamWriter writer = new StreamWriter(fileName);
+
+            foreach (Student student in students)
+            {
+                writer.WriteLine(
+                    $"{student.LastName};" +
+                    $"{student.FirstName};" +
+                    $"{student.School};" +
+                    $"{student.Score}");
+            }
+        }
+        public static List<Student> ReadStudentsFile(string fileName)
+        {
+            List<Student> students = new List<Student>();
+
+            foreach (string line in File.ReadLines(fileName))
+            {
+                string[] parts = line.Split(';');
+
+                students.Add(
+                    new Student(
+                        parts[0],
+                        parts[1],
+                        int.Parse(parts[2]),
+                        int.Parse(parts[3])));
+            }
+
+            return students;
+        }
+        #endregion
+    }
+}
